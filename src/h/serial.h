552c6f332e500cc294a6bc862e9eea34c17997f0
oculometric_ubuntu <master.scratchman@gmail.com>
2023-11-14 10:25:52 +0000

    serial!

--- src/h/serial.h
+++ src/h/serial.h
@@ -17,9 +17,27 @@ enum COM
     SCRATCH
 };
 
-inline uint8_t read_COM_register(uint32_t port, COM register);
-inline void write_COM_register(uint32_t port, COM register);
+inline uint8_t read_COM_register(uint32_t port, COM reg)
+{
+    return ((uint8_t*)port)[reg];
+}
 
-inline void set_baud_rate(uint32_t port, uint32_t br);
+inline void write_COM_register(uint32_t port, COM reg, uint8_t value)
+{
+    ((uint8_t*)port)[reg] = value;
+}
+
+inline void set_baud_rate(uint32_t port, uint16_t br)
+{
+    // set the DLAB
+    uint8_t v = read_COM_register(port, COM::LINE_CONTROL);
+    write_COM_register(port, COM::LINE_CONTROL, v | 0b10000000);
+    // place the high and low bytes
+    write_COM_register(port, COM::DATA, br & 0b11111111);
+    write_COM_register(port, COM::INTERRUPT_ENABLE, (br >> 8) & 0b11111111);
+    // clear DLAB
+    write_COM_register(port, COM::LINE_CONTROL, v);
+}
+
 inline void serial_output(uint32_t port, uint8_t c);
 inline uint8_t serial_input(uint32_t port);
