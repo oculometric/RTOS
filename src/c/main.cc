7e522b4abae546885a3eab658d8a35da61e325d2
oculometric_ubuntu <master.scratchman@gmail.com>
2023-11-26 00:16:36 +0000

    suffering oh suffering

--- src/c/main.cc
+++ src/c/main.cc
@@ -2,24 +2,19 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-#include "serial.h"
-#include "segments.h"
-
 extern "C"
-void kernel_main()
+void kernel_main(void* mbs, unsigned int magic)
 {
-
     uint16_t* terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < 25; y++) {
-		for (size_t x = 0; x < 80; x++) {
-			const size_t index = (y * 80) + x;
-			terminal_buffer[index] = (uint16_t)'X' | ((uint16_t) 0 << 8) | ((uint16_t) 4 << 12);
-		}
+
+	if (magic != 0x2BADB002)
+	{
+		terminal_buffer[0] = (uint16_t)'X' | ((uint16_t) 0 << 8) | ((uint16_t) 4 << 12);
+		return;
 	}
 
-	initialise_serial(COM1, 38400);
-	serial_output(COM1, (uint8_t*)"hello! i am alive.\n");
-	serial_output(COM1, (uint8_t*)"all work no play makes jack a dull boy\n");
+
+
 
 	uint8_t c = 'a';
 	int i = 0;
@@ -32,30 +27,6 @@ void kernel_main()
 			c++;
 		}
 		terminal_buffer[i] = (uint16_t)c | ((uint16_t) 0 << 8) | ((uint16_t) 4 << 12);
-		if (c == 255) break;
+		if (c > 'z') c = 'a';
 	}
-
-	serial_output(COM1, (uint8_t*)"initialising GDT...\n");
-
-	global_descriptor_entry gdt_address[4] = { NULL };
-	init_flat_gdt((uint8_t *)gdt_address, NULL);
-
-	serial_output(COM1, (uint8_t*)"GDT placed at ");
-	serial_output_u32(COM1, (uint32_t)gdt_address);
-	serial_output(COM1, (uint8_t*)" size ");
-	serial_output_u16(COM1, (4*sizeof(global_descriptor_entry))-1);
-	serial_output(COM1, '\n');
-
-	serial_output(COM1, (uint8_t*)"checking current CS:IP...\n");
-
-	serial_output(COM1, (uint8_t*)"now i am going to try and perform an interrupt.\n");
-
-	uint16_t kb = 0;
-	asm volatile ("clc");
-	asm volatile ("int $0x12");
-	asm volatile ("mov %0, %%ax" : "=r"(kb));
-
-	serial_output(COM1, (uint8_t*)"woo yaay!");
-
-	serial_output_u16(COM1, kb);
 }
--- src/s/boot.s
+++ src/s/boot.s
@@ -1,17 +1,3 @@
-// -------- THIS IS A VALID MULTIBOOT 2 HEADER. ENABLING IT CAUSES QEMU TO HANG ON LOADING FROM ROM ------- //
-
-// here we actually place the multiboot header. we need to use the linker.ld to ensure 
-// that this goes in the first 8kb
-//.section .multiboot_header
-//multiboot_header:
-//    .align 8
-//    .long 0xE85250D6 // magic number
-//    .long 0 // using i386 arch
-//    .long (multiboot_header_end - multiboot_header)
-//    .long -(0xE85250D6 + (multiboot_header_end - multiboot_header))
-
-//multiboot_header_end:
-
 // --------- THIS IS A VALID MULTIBOOT 1 HEADER. ------- //
 .set ALIGN,     1<<0
 .set MEMINFO,   1<<1
@@ -27,7 +13,44 @@
 .long FLAGS
 .long CHECKSUM
 
-// --- BOTH OF THE ABOVE CAUSE QEMU TO CRASH OUT WHEN LOADING FROM A CD ROM ----- //
+// --- THE ABOVE CAUSE QEMU TO CRASH OUT WHEN LOADING FROM A CD ROM ----- //
+
+.section .data
+.align 4
+
+gdt:
+
+// gdt_null is a null descriptor
+gdt_null:
+    .long 0
+    .long 0
+
+// gdt_code is a kernel code descriptor, running from 0x0 and length 0xFFFFF, ring 0, conforming code, in protected 32-bit paged
+gdt_code:
+    .int 0xFFFF
+    .int 0x0000
+    .byte 0x00
+    .byte 0b10011010
+    .byte 0b11001111
+    .byte 0x00
+
+// gdt_data is a kernel data descriptor, running from 0x0 and length 0xFFFFF, ring 0, up-growing data, in protected 32-bit paged
+gdt_data:
+    .int 0xFFFF
+    .int 0x0000
+    .byte 0x00
+    .byte 0b10010010
+    .byte 0b11001111
+    .byte 0x00
+
+gdt_end:
+
+.set GDT_LIMIT, (gdt_end-gdt)-1
+
+gdt_descriptor:
+    .int GDT_LIMIT
+    .long gdt-0x7C00
+
 
 // now we declare the stack
 .section .bss
@@ -36,18 +59,48 @@ stack_bottom:
 .skip 16384
 stack_top:
 
-
 // this is where we end up when the bootloader is done
 .section .text
 .global _start
 .type _start, @function
 _start:
+    // load the new GDT
+    lgdt (gdt_descriptor)
+
+    //int $0x10
+
+    //cli
+//4:
+    //hlt
+    //jmp 4b
+
+    // far jump to update CS, offset to the first GDT entry
+    jmpl $0x08, $post_start
+
+post_start:
+    cli
+4:
+    hlt
+    jmp 4b
+
+    // update other descriptor registers to be the second GDT entry
+    mov $0x10, %ax
+    mov %ax, %ds
+    mov %ax, %es
+    mov %ax, %fs
+    mov %ax, %gs
+    mov %ax, %ss
+
     // first we need to configure the stack. the esp register is set to
     // the top of the stack, since the stack grows down on x86
     mov $stack_top, %esp
 
+    // pass arguments to kernel_main
+    //push %eax
+    //push %ebx
+
     // jump into the C main
-    call kernel_main
+    //call kernel_main
 
     // disable interrupts and lock up the computer
     cli
