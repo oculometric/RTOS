9bf388019022feefcdf97b90aa5ece4595296be0
rtos-devbox <oculometric>
2024-01-25 14:17:08 +0000

    we made it to kernel main!

--- src/kernel_main.cpp
+++ src/kernel_main.cpp
@@ -1,6 +1,16 @@
 
 extern "C" void main()
 {
+    const char* greeting = "Hello, World!\0";
+
+
     *(char*)0xb8000 = 'R';
+    char* pointer = (char*)0xb8000;
+    int i = 0;
+    while (greeting[i] != '\0')
+    {
+        pointer[i*2] = greeting[i];
+        i++;
+    }
     return;
 }
